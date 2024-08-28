3960eeb7e52592fffb817035a9db98293cc39ad9
favo4r <favybliss262@gmail.com>
2024-08-28 23:58:52 +0000

    function that splits input into command and arguments for shell to act upon

--- input.c
+++ input.c
@@ -20,3 +20,31 @@ char *read_input(void)
 
 	return (input);
 }
+
+/**
+ * tokenize_input - Splits a string into tokens.
+ * @input: The input string.
+ *
+ * Return: An array of tokens.
+ */
+char **tokenize_input(char *input)
+{
+	char **tokens = NULL;
+	char *token;
+	size_t i = 0;
+	int bufsize = 64;
+
+	tokens = malloc(sizeof(char *) * bufsize);
+	if (!tokens)
+		return (NULL);
+
+	token = strtok(input, " \n");
+	while (token)
+	{
+		tokens[i++] = token;
+		token = strtok(NULL, " \n");
+	}
+
+	tokens[i] = NULL;
+	return (tokens);
+}
