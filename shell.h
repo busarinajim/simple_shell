432f73a59bb694f77060db3517f45f850a7eade3
favo4r <favybliss262@gmail.com>
2024-08-28 22:30:20 +0100

    header file to display prompt

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Function Prototypes */
+void sig_handler(int sig);
+
+#endif /* SHELL_H */
