27f473f3a71bd3dde15b33779bfab119aae0743c
busarinajim <busarinajim@gmail.com>
2024-08-28 22:47:18 +0000

    main function that displays prompt, waits for command and executes

--- main.c
+++ main.c
@@ -7,41 +7,42 @@
  *
  * Return: The exit status of the last command.
  */
+
 int main(int argc, char *argv[])
 {
-        char *input, **args;
-        int status = 0;
+	char *input, **args;
+	int status = 0;
 
-        (void)argc;
-        (void)argv;
+	(void)argc;
+	(void)argv;
 
-        signal(SIGINT, sig_handler);
-	
+	signal(SIGINT, sig_handler);
+
 	while (1)
-        {
-                write(STDOUT_FILENO, "$ ", 2);
-                input = read_input();
-                if (!input)
-                        break;
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		input = read_input();
+		if (!input)
+			break;
 
-                args = tokenize_input(input);
-                if (args[0] == NULL)
-                {
-                        free(input);
-                        free(args);
-                        continue;
-                }
+		args = tokenize_input(input);
+		if (args[0] == NULL)
+		{
+			free(input);
+			free(args);
+			continue;
+		}
 
-                if (strcmp(args[0], "exit") == 0)
-                        handle_exit();
-                else if (strcmp(args[0], "env") == 0)
-                        print_env();
-                else
-                        status = execute(args);
+		if (strcmp(args[0], "exit") == 0)
+			handle_exit();
+		else if (strcmp(args[0], "env") == 0)
+			print_env();
+		else
+			status = execute(args);
 
-                free(input);
-                free(args);
-        }
+		free(input);
+		free(args);
+	}
 
-        return (status);
+	return (status);
 }
