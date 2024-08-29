5f50a9d8cc69698e7c669f7f437f0d5d0102abf9
favo4r <favybliss262@gmail.com>
2024-08-29 00:06:38 +0000

    enabling the builtin env command to display current environment variables

--- builtins.c
+++ builtins.c
@@ -8,3 +8,18 @@ void handle_exit(void)
 {
 	exit(0);
 }
+
+/**
+ * print_env - Prints the current environment variables.
+ */
+void print_env(void)
+{
+	char **env = environ;
+
+	while (*env)
+	{
+		write(STDOUT_FILENO, *env, strlen(*env));
+		write(STDOUT_FILENO, "\n", 1);
+		env++;
+	}
+}
