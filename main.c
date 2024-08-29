153f336435d909616377190adb348239f6b243ce
busarinajim <busarinajim@gmail.com>
2024-08-29 00:48:50 +0000

    function added so shell handles signals

--- main.c
+++ main.c
@@ -47,7 +47,13 @@ int main(int argc, char *argv[])
 	return (status);
 }
 
+/**
+ * sig_handler - Prints a new prompt upon receiving `SIGINT`.
+ * @sig: The signal number.
+ */
 
-void sig_handler(int sig);                                                                                                                                                                                                                                                                      /**                                                                                                                                              * sig_handler - Prints a new prompt upon receiving `SIGINT`.                                                                                    * @sig: The signal number.                                                                                                                      */                                                                                                                                             void sig_handler(int sig)                                                                                                                       {
-	(void)sig;                                                                                                                                      write(STDOUT_FILENO, "\n$ ", 3);
+void sig_handler(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n$ ", 3);
 }
