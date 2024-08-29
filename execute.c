8d3738f26855e3d44f3b59592205a9a364649bd3
busarinajim <busarinajim@gmail.com>
2024-08-29 21:24:27 +0100

    executing file

--- execute.c
+++ execute.c
@@ -1,81 +1,47 @@
 #include "shell.h"
-
 /**
- * find_path - Finds the path of a command.
- * @command: The command to find.
- *
- * Return: The full path of the command or NULL if not found.
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
  */
-char *find_path(char *command)
+int execute(data_of_program *data)
 {
-	char *path, *path_dup, *dir;
-	struct stat st;
-	char full_path[1024];
+	int retval = 0, status;
+	pid_t pidd;
 
-	if (stat(command, &st) == 0)
-		return (strdup(command));
+	/* check for program in built ins */
+	retval = builtins_list(data);
+	if (retval != -1)/* if program was found in built ins */
+		return (retval);
 
-	path = getenv("PATH");
-	if (!path)
-		return (NULL);
-
-	path_dup = strdup(path);
-	dir = strtok(path_dup, ":");
-	while (dir)
-	{
-		snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-		if (stat(full_path, &st) == 0)
-		{
-			free(path_dup);
-			return (strdup(full_path));
-		}
-		dir = strtok(NULL, ":");
-	}
-
-	free(path_dup);
-	return (NULL);
-}
-
-/**
- * execute - Executes a command in a child process
- * @args: An array of arguments.
- *
- * Return: 0 on success, or an error code.
- */
-int execute(char **args)
-{
-	pid_t child_pid;
-	char *command;
-	int status;
-
-	command = find_path(args[0]);
-	if (!command)
-	{
-		perror(args[0]);
-		return (127);
-	}
-
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error");
-		free(command);
-		return (1);
-	}
-	if (child_pid == 0)
-	{
-		if (execve(command, args, environ) == -1)
-		{
-			perror(args[0]);
-			free(command);
-			exit(1);
-		}
+	/* check for program file system */
+	retval = find_program(data);
+	if (retval)
+	{/* if program not found */
+		return (retval);
 	}
 	else
-	{
-		wait(&status);
+	{/* if program was found */
+		pidd = fork(); /* create a child process */
+		if (pidd == -1)
+		{ /* if the fork call failed */
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{/* I am the child process, I execute the program*/
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1) /* if error when execve*/
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{/* I am the father, I wait and check the exit status of the child */
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
 	}
-
-	free(command);
-	return (WEXITSTATUS(status));
+	return (0);
 }
